55d74f880440d86cf16297ad15500dbbaf26e4b3
Ilnur Ziganshin <zig.i@bk.ru>
2025-03-14 17:38:24 +0300

    config.py -- добавил логирование, обработку ошибок

--- config.py
+++ config.py
@@ -1,12 +1,20 @@
 import os
+import logging
 from dotenv import load_dotenv
 
-# Загружаем .env
-load_dotenv()
+logging.basicConfig(level=logging.INFO)
 
-# API-ключи
-TGBOT_API_KEY = os.getenv("TGBOT_API_KEY")
-WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
+try:
+    # Загружаем .env
+    load_dotenv()
+    
+    # API-ключи
+    TGBOT_API_KEY = os.getenv("TGBOT_API_KEY")
+    WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
 
-if not TGBOT_API_KEY or not WEATHER_API_KEY:
-    raise ValueError("❌ Ошибка! Отсутствует TGBOT_API_KEY или WEATHER_API_KEY в .env")
+    if not TGBOT_API_KEY or not WEATHER_API_KEY:
+        raise ValueError("❌ Ошибка! Отсутствует TGBOT_API_KEY или WEATHER_API_KEY в .env")
+
+except Exception as ex:
+    logging.error(f"❌ Ошибка при загрузке конфигурации: {e}")
+    raise  # Останавливаем выполнение программы
